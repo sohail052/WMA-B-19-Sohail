f6146f77ed81ab26a1a7f39a89f7af204315b3cd
Muhammad Sohail <msohail.saleem100@gmail.com>
2026-01-30 00:15:12 +0500

    30-Jan-2026: Assignment upload table table print is console

---
+++ Assignments_and_Work/AlertJsTimeTabe/logic.js
@@ -0,0 +1,6 @@
+let inputUser = +prompt("Enter a number");
+let countNum = +prompt("How much iterations you want?");
+
+for (i = 1; i <= countNum; i++) {
+    console.log(inputUser +" x "+i+" = "+inputUser*i);
+}
