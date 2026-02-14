6ba063ca6f472974f401f20768bfa3f6262a3a29
Muhammad Sohail <msohail.saleem100@gmail.com>
2026-02-14 20:52:10 +0500

    14-02-2026: Lecture upload.

---
+++ Assignments_and_Work/PracticeJavaScriptWMA/practice.js
@@ -0,0 +1,65 @@
+// This is Sir's file of lectures conducted on 07-02-2026
+
+
+
+// const today = new Date();
+// const graduationDay = new Date();
+
+// const todayYear = today.getFullYear();
+// console.log(today,todayYear)
+// graduationDay.setFullYear(todayYear + 1);
+// console.log(graduationDay)
+
+// const word = "algebra";
+
+// let guessWord = word.split("");
+// guessWord[1] = "_";
+// guessWord[3] = "_";
+// guessWord[5] = "_";
+// guessWord = guessWord.join("");
+
+// alert(guessWord);
+// const date = new Date();
+// const startingTime = date.getTime();
+// console.log(startingTime);
+
+// const userAnswers = [];
+
+// for (let i = 0; i < 3; i++) {
+//   const answer = prompt("Enter the first letter");
+//   userAnswers.push(answer);
+// }
+
+// const finishingDate = new Date();
+// const finishingTime = finishingDate.getTime();
+
+// const timeConsumedInMS = finishingTime - startingTime;
+
+// const timeConsumedInSecs = timeConsumedInMS / 1000;
+
+// let userWord = guessWord;
+// for (let i = 0; i < 3; i++) {
+//   userWord = userWord.replace("_", userAnswers[i]);
+// }
+
+// if (userWord.toLowerCase() !== word) alert(" Ohho, Wrong word");
+// else if (timeConsumedInSecs > 5) alert(" Ohho, too slow");
+// else alert("You won, don't think you're genuis");
+
+// const doomsday = new Date("June 30, 2035");
+// console.log(doomsday);
+
+
+
+// // function greetUser(firstName, lastName) {
+// //   const greeting = "Hi " + firstName + " " + lastName;
+// //   alert(greeting);
+// // }
+
+// // const userName = prompt("Enter your name");
+// // // invoke / call
+// // debugger;
+// // greetUser("Muneer","");
+
+// // debugger;
+// // greetUser("Ali", "Shabbir");
